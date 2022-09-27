5a678a69aaeb0df7bdb20c7b7af5741938d15d3b
Sid Lin <sydneyl@qualtrics.com>
2022-09-27 16:19:42 -0700

    save

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-console.log('hello')
-const x = Document.getElementById('passwordInput');
-console.log(x);
+const input = document.getElementById('passwordInput');
+input.addEventListener('keydown', (e) => {
+	if (e.key === 'Enter') {
+		const entered = input.value;
+		const password = window.atob('MDQvMTcvMjAyMg==')
+		if (entered === password) {
+			window.location.href = 'https://www.youtube.com/channel/UC7uNb1YfzrnHBixDq-TFgyw';
+		}
+	}
+});
