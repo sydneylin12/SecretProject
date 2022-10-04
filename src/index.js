d6ff1f56b0bc18d950e6bfa52645d9dcf76d1395
Sid Lin <sydney.lin12@gmail.com>
2022-10-04 00:49:06 -0700

    redirect:

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const input = document.getElementById('passwordInput');
 const text = document.getElementById('clickMe');
 const password = window.atob('MDQvMTcvMjAyMg==')
 const encodedResult = 'aHR0cHM6Ly95b3V0dS5iZS9rTUExWEY5NjVFVQ==';
-const storyPage = 'src/story.html';
+const path = 'https://sydneylin12.github.io/SecretProject/';
+const storyPage = 'story.html';
 let count = 0;
+
+const isLocalBuild = () => {
+	return window.location.href.includes('file');
+}
 
 input.addEventListener('keydown', (e) => {
 	if (e.key === 'Enter') {
@@ -18,12 +23,11 @@ input.addEventListener('keydown', (e) => {
 
 text.addEventListener('click', (e) => {
 	count = count + 1;
-	console.log(count);
 	if (e.target === text) {
 		count += 1;
 		if (count >= 50) {
-			//window.location.href = window.atob(encodedResult);
-			window.location.href = storyPage;
+			const urlPrefix = isLocalBuild() ? '' : path;
+			window.location.href = urlPrefix + storyPage;
 		}
 	}
 });
