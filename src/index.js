96e5c8860f7c088c34ff6622cf1617dda0e8f97c
Sid Lin <sydneyl@qualtrics.com>
2022-10-04 12:41:20 -0700

    date protection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const input = document.getElementById('passwordInput');
 const text = document.getElementById('clickMe');
-const password = window.atob('MDQvMTcvMjAyMg==')
-const encodedResult = 'aHR0cHM6Ly95b3V0dS5iZS9rTUExWEY5NjVFVQ==';
+const password = window.atob('Mk4xNkE3NDgyQzU/VFU=');
+const secretURL = window.atob('aHR0cHM6Ly95b3V0dS5iZS9rTUExWEY5NjVFVQ==');
+const rickRoll = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 const path = 'https://sydneylin12.github.io/SecretProject/';
 const storyPage = 'story.html';
 let count = 0;
@@ -10,13 +11,18 @@ const isLocalBuild = () => {
 	return window.location.href.includes('file');
 }
 
+const isCorrectDate = () => {
+	const day = new Date();
+	return day.getDate() === 17 && day.getMonth() === 9;
+}
+
 input.addEventListener('keydown', (e) => {
 	if (e.key === 'Enter') {
 		const entered = input.value;
 		if (entered === password) {
-			console.log(encodedResult)
+			window.location.href = isCorrectDate() ? secretURL : rickRoll;
 		} else {
-			alert('Nice try!')
+			alert('Wrong passcode, nice try!')
 		}
 	}
 });
