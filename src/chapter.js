536a4052fe2142a826bb7b628353212388d9de5f
Sid Lin <sydneyl@qualtrics.com>
2023-09-21 14:09:54 -0400

    save

--- src/chapter.js
+++ src/chapter.js
@@ -5,6 +5,13 @@ const content = document.getElementById('content');
 const params = new URLSearchParams(location.search);
 const chapterNumber = parseInt(params.get('chapter'));
 
+/**
+ * Usage:
+ * Go to gmail -> click on email -> show original -> copy to clipboard
+ * Paste the content you copied into <chapterNum>.txt
+ * 
+ * This will parse out the unwanted email filler and preserve the email's HTML format
+ */
 const getAndLoadChapterData = async () => {
 	const fileName = `${chapterNumber}.txt`;
 	const response = await fetch(`https://raw.githubusercontent.com/sydneylin12/SecretProject/main/chapters/${fileName}`);
