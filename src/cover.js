b3611a589f95cad8dedac4f31147245d6b042f4e
Sid Lin <sydneyl@qualtrics.com>
2023-09-22 16:55:01 -0400

    save

--- src/cover.js
+++ src/cover.js
@@ -1,9 +1,4 @@
-const b1 = document.getElementById('book1');
-const b2 = document.getElementById('book2');
-const b3 = document.getElementById('book3');
-
-[b1, b2, b3].forEach((element) => {
-	element.addEventListener('click', () => {
-		window.location.href = 'table-of-contents.html';
-	});
+const b1 = document.getElementById('book');
+b1.addEventListener('click', () => {
+	window.location.href = 'table-of-contents.html';
 });
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const book = document.getElementById('bookText');
 
 const WangXian = 'https://www.youtube.com/watch?v=8jQ_f5p3p9o&ab_channel=MyPianoAcademia';
 const storyPage = 'story.html';
-const covers = 'main-view.html';
+const covers = 'book-cover.html';
 
 book.addEventListener('click', () => {
 	window.location.href = covers;
