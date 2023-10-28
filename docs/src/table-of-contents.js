4418b2bbd84053680f957f22ada3b334f0db4fe0
Sid Lin <sydney.lin12@gmail.com>
2023-10-28 21:26:29 +0000

    save

--- docs/src/table-of-contents.js
+++ docs/src/table-of-contents.js
@@ -1,15 +1,30 @@
 const el = document.getElementById('list');
 
-const CHAPTER_COUNT = 43;
-for (let i = 0; i < CHAPTER_COUNT; i++) {
-	const child = document.createElement('li');
-	child.innerHTML = `Chapter ${i+1}`
-	el.appendChild(child);
+const doesChapterExist = async (chapterNumber) => {
+	const fileName = `${chapterNumber}.txt`;
+	const response = await fetch(`https://raw.githubusercontent.com/sydneylin12/SecretProject/main/chapters/${fileName}`);
+	const res = await response.status();
+	return res === 200;
+};
+
+let chapterNumber = 1;
+let lastChapterFound = false;
+while (!lastChapterFound) {
+	const exists = await doesChapterExist(chapterNumber);
+	if (!exists) {
+		lastChapterFound = true;
+	} else {
+		const child = document.createElement('li');
+		child.innerHTML = `Chapter ${i}`
+		el.appendChild(child);
+		chapterNumber += 1;
+	}
 }
 
 // Redirect to the template with chapter number as a query param
 el.addEventListener('click', (e) => {
 	const text = e.target.innerHTML;
+	// Double digit numbers have a title of 'Chapter XX' which is <= 10 characters
 	if (text.includes('Chapter') && text.length <= 10) {
 		const chapterNumber = parseInt(text.split(' ').pop());
 		window.location.href = `chapter.html?chapter=${chapterNumber}`;
